aafc4dda683d2c6366f605ce7ba5baa02d93cf08
Nacho <Dyrien7@gmail.com>
2023-01-02 19:54:01 -0300

    metodos Mongo Productos

--- src/Daos/Productos/ProductosMongo.js
+++ src/Daos/Productos/ProductosMongo.js
@@ -1,35 +1,59 @@
-import mongoose from 'mongoose';
-import { modeloProductos } from '../mongo/scripts/scripts.js';
+import mongoose from "mongoose";
+import { modeloProductos } from "../mongo/scripts/scripts.js";
 
-const mongoURL = "mongodb+srv://Dyrien07:Nacho115173@cluster0.pvolnwm.mongodb.net/Ecommers?retryWrites=true&w=majority"
+const mongoURL =
+  "mongodb+srv://Dyrien07:Nacho115173@cluster0.pvolnwm.mongodb.net/Ecommers?retryWrites=true&w=majority";
 
-
-
-mongoose.connect(mongoURL,{
+mongoose.connect(
+  mongoURL,
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}, error=>{
-    if(error) return console.log(error);
+    useUnifiedTopology: true,
+  },
+  (error) => {
+    if (error) return console.log(error);
     console.log("Coneccion Exitosa");
-})
+  }
+);
 
-const addProduct = async()=>{
+export const addProduct = async (unProducto) => {
+  try {
+    await modeloProductos.insert(unProducto);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const deleteProduct = async (unProducto) => {
+  try {
+    await modeloProductos.delete(unProducto);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+export const updateProduct = async (unProducto) => {
+  try {
+    await modeloProductos.update(unProducto);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const getProduct = async (ID) => {
+    try {
+      const unProducto =   await modeloProductos.findOne({id: ID});
+        return unProducto
+    }catch (err) {
+        console.log(err.message);
+    }
 
 };
 
-const deleteProduct = async()=>{
-
+export const getAllProduct = async () => {
+  try {
+    let preductos = await modeloProductos.find();
+    console.log(preductos);
+  } catch (err) {
+    console.log(err.message);
+  }
 };
-const updateProduct = async()=>{
-
-};
-
-const getProduct = async()=>{
-
-};
-
-const getAllProduct = async()=>{
-let preductos = await modeloProductos.find();
-console.log(preductos)
-};
-
